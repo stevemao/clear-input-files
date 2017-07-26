9f4f9822706a4a62d1bda3ed7989314690900369
Steve Mao <maochenyan@gmail.com>
2017-07-26 13:04:42 +1000

    http://stackoverflow.com/a/24608023/194065

---
+++ index.js
@@ -0,0 +1,17 @@
+// http://stackoverflow.com/a/24608023/194065
+function clearInputFile(f){
+    if(f.value){
+        try{
+            f.value = ''; //for IE11, latest Chrome/Firefox/Opera...
+        }catch(err){ }
+        if(f.value){ //for IE5 ~ IE10
+            var form = document.createElement('form'),
+                parentNode = f.parentNode, ref = f.nextSibling;
+            form.appendChild(f);
+            form.reset();
+            parentNode.insertBefore(f,ref);
+        }
+    }
+}
+
+module.exports = clearInputFile
